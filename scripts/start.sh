a4c4221e76f30f064bf2567435af6e3fd2ad5e46
박진형 <skill1218@naver.com>
2025-09-09 13:06:03 +0900

    fix start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/libs/back-1.0.0.jar $JAR
+cp $ROOT_PATH/libs/back-1.0.0.-SNAPSHOTjar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR > $APP_LOG 2> $ERROR_LOG &
